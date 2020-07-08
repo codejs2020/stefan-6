6a4abe0f332b396c2f8552c1cad522cc78b45d65
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2020-07-08 20:40:07 +0200

    cetvrti zadatak dodat

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@
 const slova = ['a', 'b', 'c']
 // eslint-disable-next-line
 console.log(slova)
+
 // drugi zadatak
 const brojevi = [1, 2, 3]
 // eslint-disable-next-line
 console.log(brojevi)
+
+// cetvrti zadatak
+const niz = new Array(50)
+let broj
+for (let i = 0; i <= niz.length; i++) {
+  while (broj < 1000000) {
+    broj = Math.ceil(Math.random() * 10000000)
+  }
+  niz[i] = broj
+}
