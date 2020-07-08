5bba435c745fdb1ec5f5497c7d89467f929e7e58
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2020-07-08 20:28:10 +0200

    prvi i drugi zadatak

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-const a = 3
-
+// prvi zadatak
+const slova = ['a', 'b', 'c']
 // eslint-disable-next-line
-console.log('a', a)
+console.log(slova)
+// drugi zadatak
+const brojevi = [1, 2, 3]
+// eslint-disable-next-line
+console.log(brojevi)
