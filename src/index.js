7a0d956b98205ad812b6ad78af85db687c363462
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2020-07-08 20:50:38 +0200

    dodat komentar u 4. zadatak

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ console.log(brojevi)
 
 // cetvrti zadatak
 const niz = new Array(50)
-let broj
+let broj = 1
+// Da ima neku vrednost koju ce u prvom koraku da uporedi u while petlji
 for (let i = 0; i <= niz.length; i++) {
   while (broj < 1000000) {
     broj = Math.ceil(Math.random() * 10000000)
